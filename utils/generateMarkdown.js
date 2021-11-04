55db9cf5ec7494b1e47aa0aa5d8e8a1660890f4c
Ami Asokumar <gasokumar@gmail.com>
2021-11-04 08:31:12 -0400

    added links to renderlicenseLink "

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,45 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case "MIT":
+
+    case "MPL-2.0":
+
+    case "Apache-2.0":
+
+    case "BSD-3-Clause":
+
+    case "BSD-2-Clause":
+
+    case "GPL":
+
+    case "LGPL":
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case "MIT":
+      return "https://opensource.org/licenses/MIT";
+    case "MPL-2.0":
+      return "https://opensource.org/licenses/MPL-2.0";
+    case "Apache-2.0":
+      return "https://opensource.org/licenses/Apache-2.0";
+    case "BSD-3-Clause":
+      return "https://opensource.org/licenses/BSD-3-Clause";
+    case "BSD-2-Clause":
+      return "https://opensource.org/licenses/BSD-2-Clause";
+    case "GPL":
+      return "https://opensource.org/licenses/gpl-license";
+    case "LGPL":
+      return "https://opensource.org/licenses/lgpl-license";
+    default:
+      "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -40,7 +75,7 @@ function generateMarkdown(responses) {
 
   ## Questions
   The creator's github profile is github.com/${responses.github}.
-  You can reach out to their email, ${responses.email} if you have any questions.
+  You can reach out to their email, ${responses.email}, if you have any questions.
 
 `;
 }

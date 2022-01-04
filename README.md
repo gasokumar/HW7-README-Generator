90a3f362e95402b7bab057831dab3e4c0dff185f
Ami Asokumar <gasokumar@gmail.com>
2022-01-04 02:51:26 -0500

    got badges and badge link to render in license description

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# Project Title: my test
+# Project Title: README Generator
   ## Description
-  testing
+  ![badge](https://img.shields.io/badge/license-MIT-success)
+  
+  This is an application that generates a markdown README file for a user after a series of prompts have been answered in the command line.
+  
+  
   ## Table of Contents
 - [Installation](#installation)
 - [Usage](#usage)
@@ -10,18 +14,19 @@
 - [Questions](#questions)
 
   ## Installation
-  no
+  The files for this application must be downloaded in order to use it.
   ## Usage
-  figure it out
+  Once the files have been downloaded, node index.js must be typed into the CLI  in order to invoke the application.
   ## Contributing
-  nope
+  Contributions are welcome!
   ## Tests
-  no
+  No tests!
   ## License
-  MIT
-  //Add notice here
+  This application is covered under the MIT license.
+  https://opensource.org/licenses/MIT
+  
 
   ## Questions
-  The creator's github profile is github.com/gasokumar
-  You can reach out to their email, gasokumar@gmail.com if you have any questions.
+  The creator's github profile is github.com/gasokumar.
+  You can reach out to their email, gasokumar@gmail.com, if you have any questions.
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,19 +3,28 @@
 function renderLicenseBadge(license) {
   switch (license) {
     case "MIT":
-      return;
+      return `![badge](https://img.shields.io/badge/license-MIT-success)`;
 
     case "MPL-2.0":
+      return `![badge](https://img.shields.io/badge/license-MPL2.0-success)`;
 
     case "Apache-2.0":
+      return `![badge](https://img.shields.io/badge/license-Apache2.0-success)`;
 
     case "BSD-3-Clause":
+      return `![badge](https://img.shields.io/badge/license-BSD3Clause-success)`;
 
     case "BSD-2-Clause":
+      return `![badge](https://img.shields.io/badge/license-BSD2Clause-success)`;
 
     case "GPL":
+      return `![badge](https://img.shields.io/badge/license-GPL-success)`;
 
     case "LGPL":
+      return `![badge](https://img.shields.io/badge/license-LGPL-success)`;
+
+    default:
+      return "";
   }
 }
 
@@ -45,15 +54,26 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  return;
+  if (license) {
+    const link = renderLicenseLink(license);
+    return `${link}`;
+  } else {
+    return ``;
+  }
 }
 
 // TODO: Create a function to generate markdown for README
 //This is how the README will be styled. # = h1, ## = h2, etc.
 function generateMarkdown(responses) {
+  const badge = renderLicenseBadge(responses.license);
+  const licenseSection = renderLicenseSection(responses.license);
   return `# Project Title: ${responses.title}
   ## Description
+  ${badge}
+  
   ${responses.description}
+  
+  
   ## Table of Contents
 - [Installation](#installation)
 - [Usage](#usage)
@@ -71,8 +91,9 @@ function generateMarkdown(responses) {
   ## Tests
   ${responses.tests}
   ## License
-  ${responses.license}
-  //Add notice here
+  This application is covered under the ${responses.license} license.
+  ${licenseSection}
+  
 
   ## Questions
   The creator's github profile is github.com/${responses.github}.
